bc265d206d684db4918f6f68712eed255b594306
Bradley Showen <BShowen@me.com>
2022-10-13 16:57:51 -0400

    Add hack to get this to work on Safari

--- src/index.js
+++ src/index.js
@@ -21,8 +21,12 @@ snakeRoot.render(
 );
 
 window.onload = () => {
-  new Game().start();
+  // A hack to get this to work on Safari.
+  setTimeout(() => {
+    new Game().start();
+  }, 500);
 };
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
